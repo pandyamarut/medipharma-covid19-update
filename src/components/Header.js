2ef8eb9fc9616c024263941ce368eda187f0003d
user <pandyamarut@gmail.com>
2020-04-04 15:36:22 +0530

    initial commit.

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+import Container from 'components/Container';
+
+const Header = () => {
+  return (
+    <header>
+      <Container type="content">
+        <p>MediPharma Connect initiative</p>
+        <ul>
+          <li>
+          </li>
+          <li>
+          </li>
+        </ul>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
